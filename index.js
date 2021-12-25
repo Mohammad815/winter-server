09ad5d945738fe14b4fd816327f00f24e86305c8
Mohammad815 <mohammadforhad176@gmail.com>
2021-12-26 01:26:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ns6st.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+      await client.connect();
+      console.log("database connected successfully")
+
+      const database = client.db('winter_clothes');
+      const productsCollection = database.collection('products');
+
+      //add ProductCollection
+      app.post("/AddProduct", async (req, res) => {
+         console.log(req.body);
+        const result = await productsCollection.insertOne(req.body);
+        res.send(result);
+     });
+
+        // get all products
+        app.get("/allProducts", async (req, res) => {
+            const result = await productsCollection.find({}).toArray();
+            res.send(result);
+        });
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Winter shop!')
+})
+
+app.listen(port, () => {
+  console.log(`listening at ${port}`)
+})
